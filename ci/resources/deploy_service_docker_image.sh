57db77d913a9215c35fb98c236b5c55657c4a176
Antoine Nicolas <antoine.nicolas@gmail.com>
2022-07-19 13:56:52 +0200

    Trying switching to new CI pod

--- ci/resources/deploy_service_docker_image.sh
+++ ci/resources/deploy_service_docker_image.sh
@@ -4,14 +4,13 @@ set -xe
 
 # Builds & deploys TCOMPv0 Docker image and pushes it on Talend's registry
 # Two tags are set, the version's and latest
-# $1: The host of the local Docker registry where the Docker image has been built
-# $2: The host of the Talend Docker registry
-# $3: The project's version
+# $1: The host of the Talend Docker registry
+# $2: The project's version
+# $3: The path to the maven settings file
 main () {
-  local localRegistryHost="${1:?Missing local registry host}"
-  local talendRegistryHost="${2:?Missing talend registry host}"
-  local projectVersion="${3:?Missing project version}"
-  local mavenSettingsPath="${4:?Missing maven settings path}"
+  local talendRegistryHost="${1:?Missing talend registry host}"
+  local projectVersion="${2:?Missing project version}"
+  local mavenSettingsPath="${3:?Missing maven settings path}"
 
   local dockerImageName="tcomp-components-api-service-rest-all-components"
   local dockerImageTimestamp=$(date '+%Y%m%d-%H%M%S')
@@ -22,18 +21,20 @@ main () {
   (
     cd services/components-api-service-rest-all-components
 
+    # Service image is directly built as "${talendRegistryHost}/${dockerImageName}" thanks to the docker-maven-plugin
+    # configuration. Probably not ideal...
     mvn package \
     --define 'skipTests=true' \
     --define "docker.image.tag=${dockerImageTag}" \
     --activate-profiles docker
 
-    printf "Successfully built image %s:%s" "${dockerImageName}:${dockerImageTag}"
+    printf "Successfully built image %s:%s\n" "${dockerImageName}" "${dockerImageTag}"
 
     printf "Docker tag & push to %s\n" "${talendRegistryHost}"
-    docker tag  "${localRegistryHost}/${dockerImageName}:${dockerImageTag}" "${talendRegistryHost}/${dockerImageName}:${dockerImageTag}"
+    docker tag  "${talendRegistryHost}/${dockerImageName}:${dockerImageTag}" "${talendRegistryHost}/${dockerImageName}:${dockerImageTag}"
     docker push "${talendRegistryHost}/${dockerImageName}:${dockerImageTag}"
 
-    docker tag "${localRegistryHost}/${dockerImageName}:${dockerImageTag}" "${talendRegistryHost}/${dockerImageName}:latest"
+    docker tag "${talendRegistryHost}/${dockerImageName}:${dockerImageTag}" "${talendRegistryHost}/${dockerImageName}:latest"
     docker push "${talendRegistryHost}/${dockerImageName}:latest"
   )
 }
